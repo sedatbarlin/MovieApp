365eea77a6018bef27e1eddb7494c1821a944366
Sedat Barlin <barlinsedat@gmail.com>
2023-03-23 01:51:52 +0300

    movie app

---
+++ MovieApp/FavoritesVC.swift
@@ -0,0 +1,19 @@
+//
+//  FavoritesVC.swift
+//  MovieApp
+//
+//  Created by Sedat on 21.03.2023.
+//
+
+import UIKit
+
+class FavoritesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    
+}
---
+++ MovieApp/HomeVC.swift
@@ -0,0 +1,20 @@
+//
+//  HomeVC.swift
+//  MovieApp
+//
+//  Created by Sedat on 21.03.2023.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    
+    
+}
